12105d6cbbfda4a1c8fa43cf625e01362358b2dd
Spring Raindrop <SpringRaindrop@vivaldi.net>
2020-02-06 13:13:43 +0000

    Rewrite load-new-toots one more time
    New taken shouldn't be equal to what it was, as we may load the last
    toots from a page we already took toots from. Add docs, unify actions
    for when there's a next url and when there isn't.

--- src/no_boosts/core.cljs
+++ src/no_boosts/core.cljs
@@ -102,7 +102,9 @@
                                                      (add-page page))))))
 
 (defn load-new-toots [url number taken handler & [s]]
-  "Returns a seq of toots, next page url and the number of toots taken from it."
+  "Loads `number` new toots starting from `url`, filtering boosts and other
+  unneeded stuff. `taken` is how many toots from the first page to discard.
+  Returns a seq of toots, next page url and the number of toots taken from it."
   (get-page url (fn [page]
                   (let [next-url (get page "next")
                         activities (get page "orderedItems")
@@ -111,16 +113,25 @@
                                     (if (nil? s) '() s) ; '() on first iteration, s after
                                     (nthrest new-toots taken))]
 
-                    (if-not (clojure.string/blank? next-url)
-                      (if (< (count all-toots) number)
-                        (load-new-toots next-url number 0 handler all-toots)
-                        (let [new-taken (- number (count s))]
-                          (if (> new-taken 0)
-                            (handler (take number all-toots) url new-taken)
-                            (handler (take number all-toots) next-url 0))))
-                      (if (> (count all-toots) number)
-                        (handler (take number all-toots) url (- number (count s)))
-                        (handler all-toots "" 0)))))))
+                    ; If we need to load new toots and it's possible,
+                    (if (and (not (clojure.string/blank? next-url)) (< (count all-toots) number))
+                      ; do it.
+                      (load-new-toots next-url number 0 handler all-toots)
+                      ; Otherwise, count how many toots are we gonna take from the page.
+                      ; If we already took some from here, add it.
+                      (let [new-taken (+ (- (min number (count all-toots)) (count s)) taken)]
+                        (if (not= new-taken (count new-toots))
+                          ; If we only use some toots, tell how much and return
+                          ; the same URL, as it has more.
+                          (handler (take number all-toots) url new-taken)
+                          ; If we take a full page bc we need it all,
+                          ; all-toots = number, taken is number - s = new-toots.
+                          ; We then return the next URL, as current is useless.
+                          ;
+                          ; If we take the full page bc next is unavailable, taken
+                          ; will be all-toots - s = new-toots. We return the
+                          ; next URL which is nil, zero toots taken from there.
+                          (handler all-toots next-url 0))))))))
 
 (defn next-page [e]
   (.preventDefault e)
